cba173f4bfe05c6c98f48eb8db74d9efdfa23b22
dongyue <dongy@esage.cn>
2015-08-24 15:33:47 +0800

    better-better.me website power by yoeman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name betterBettermeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the betterBettermeApp
+ */
+angular.module('betterBettermeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name betterBettermeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the betterBettermeApp
+ */
+angular.module('betterBettermeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
